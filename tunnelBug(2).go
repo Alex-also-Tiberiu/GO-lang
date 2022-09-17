108f99e964c14b44c741d9d594914d9ea92b7f3c
Alex_also_Tiberiu <76185311+Sputnik10g@users.noreply.github.com>
2022-09-17 19:56:06 +0200

    Update tunnelBug(2).go

--- tunnelBug(2).go
+++ tunnelBug(2).go
@@ -1,3 +1,19 @@
+/**
+Vi è dato un programma (che trovate su Moodle: tunnelBug.go) che simuli la seguente situazione:
+● Ci sono due gruppi di palline G1 e G2 in due luoghi diversi L1 e L2 uniti da un tunnel. In L1 e in 
+L2 ci sono due persone P1 e P2.
+● La persona P1 vuole lanciare tutte le palline in G1 da L1 a L2, e viceversa P2 vuole lanciare 
+lanciare le palline in G2 da L2 a L1.
+● Il tunnel è stretto, ci può passare solo una pallina alla volta. Se due palline vengono lanciate nel 
+tunnel contemporaneamente, tornano al punto di partenza (immediatamente). Una pallina 
+attraversa il tunnel in un secondo (time.Sleep(time.Second)).
+● Una persona non può lanciare una pallina finché quella che ha lanciato precedentemente non è 
+arrivata a destinazione o non ha incontrato una pallina che andava in senso contrario.
+● Ci sono due gruppi di palline e due routine che lanciano le palline da un capo all’altro. Le routine 
+attendono un tempo casuale (time.Sleep(time.Duration(rand.Intn(2))*time.Second)) prima di 
+lanciare una nuova palla. Le routine finiscono quando nel relativo gruppo le palline finiscono.
+*/
+
 package main
 
 import (
